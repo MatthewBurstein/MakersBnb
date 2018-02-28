81cd8784fab21321a4b1d65cbe1433805c2671d5
Daniel Keen <keendaniel01@gmail.com>
2018-02-28 11:58:26 +0000

    Can serve up index.html. Scripts won't load.

--- api/routes/makersBnbRoutes.js
+++ api/routes/makersBnbRoutes.js
@@ -1,16 +1,22 @@
 'use strict';
 module.exports = function(app) {
   var makersBnb = require('../controllers/makersBnbController');
+  // var index = require('../index.html');
 
   // todoList Routes
   app
     .route('/properties')
     .get(makersBnb.list_all_properties)
     .post(makersBnb.create_a_property);
-  // 
+  //
   // app
   //   .route('/tasks/:taskId')
   //   .get(todoList.read_a_task)
   //   .put(todoList.update_a_task)
   //   .delete(todoList.delete_a_task);
+  app.get('/', function(req, res) {
+    res.sendFile(
+      '/Users/danielkeen/Projects/week_6/React/MakersBnb/index.html'
+    );
+  });
 };
