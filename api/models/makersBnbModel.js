bc903b9ca9ce98f5ea36fa2044491070fcc7f934
Matt Burstein <j.matthew.burstein@gmail.com>
2018-02-28 10:57:05 +0000

    Minor cosmetic changes

--- api/models/makersBnbModel.js
+++ api/models/makersBnbModel.js
@@ -14,23 +14,10 @@ var PropertySchema = new Schema({
     type: String,
     required: 'Please provide your email address'
   },
-  // name: {
-  //   type: String,
-  //   required: 'Kindly enter the name of the task'
-  // },
   Created_date: {
     type: Date,
     default: Date.now
-  },
-  // status: {
-  //   type: [
-  //     {
-  //       type: String,
-  //       enum: ['pending', 'ongoing', 'completed']
-  //     }
-  //   ],
-  //   default: ['pending']
-  // }
+  }
 });
 
 module.exports = mongoose.model('Properties', PropertySchema);
