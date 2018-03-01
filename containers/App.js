fad4c7cec713ebe14e8823251de26939a368b072
Daniel Keen <keendaniel01@gmail.com>
2018-03-01 12:07:40 +0000

    Can print out databases emails using react and axios

--- containers/App.js
+++ containers/App.js
@@ -1,7 +1,34 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      properties: []
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    axios.get('http://localhost:3000/properties').then(response => {
+      this.setState({ properties: response.data });
+      console.log('Success');
+    });
+  }
+
   render() {
-    return <p>This is my new react app</p>;
+    return (
+      <div>
+        <p>This is my new react app</p>
+        <button onClick={this.handleClick}>Click to get properties</button>
+        <ul>
+          {this.state.properties.map(property => (
+            <li key={property._id}>{property.email}</li>
+          ))}
+        </ul>
+      </div>
+    );
   }
 }
