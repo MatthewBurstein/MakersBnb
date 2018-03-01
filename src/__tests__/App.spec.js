3f18e6ffd5e4c5cbe0a4ac1f49ed7a466655ae78
jose <jennymarin1989@hotmail.com>
2018-03-01 18:14:16 +0000

    Testing with jest

---
+++ src/__tests__/App.spec.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Enzyme, { shallow, mount, render } from 'enzyme';
+import ReactDOM from 'react-dom';
+import App from '../App';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import 'jest-enzyme';
+
+Enzyme.configure({ adapter: new Adapter() });
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+it('renders welcome message', () => {
+  const wrapper = shallow(<App />);
+  const welcome = <h2>Welcome!!!</h2>;
+  expect(wrapper).toContainReact(welcome)
+});
