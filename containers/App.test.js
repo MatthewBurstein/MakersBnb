c1fb70b95ea3f14f7366f57caf77be17aaafccb4
carlostrapet <trapet_llamas@hotmail.com>
2018-03-02 12:48:24 +0000

    added test for button. fixed bugs

--- containers/App.test.js
+++ containers/App.test.js
@@ -1,22 +1,28 @@
-import React from ‘react’;
-import Enzyme, { shallow, mount, render } from ‘enzyme’;
-import ReactDOM from ‘react-dom’;
-import App from ‘./App’;
-import { configure } from ‘enzyme’;
-import Adapter from ‘enzyme-adapter-react-16’;
-import ‘jest-enzyme’;
+import React from 'react';
+import Enzyme, { shallow, mount, render } from 'enzyme';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import 'jest-enzyme';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-it(‘renders without crashing’, () => {
-  const div = document.createElement(‘div’);
+it('renders without crashing', () => {
+  const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
-it(‘renders welcome message’, () => {
+it('renders welcome message', () => {
   const wrapper = shallow(<App />);
-  const welcome = <h2>Welcome to React</h2>;
+  const welcome = <p>This is my new react app</p>;
   expect(wrapper).toContainReact(welcome)
+});
+
+describe('Button', function() {
+  it('should be defined', function() {
+    expect(Button).toBeDefined();
+  });
 
 });
