48ca6878e11e3cc3de19ca18fe3be16bd3c9df49
Daniel Keen <keendaniel01@gmail.com>
2018-03-02 15:07:15 +0000

    Test for form rendering failing

--- containers/Button.js
+++ containers/Button.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 
 export default class Button extends Component {
-  render() {
+  constructor() {
+    super();
+    this.renderForm = this.renderForm.bind(this);
+  }
+  renderForm() {
     return (
-      <div>
-        <button>Add a new property</button>
-      </div>
+      <form>
+        <input type="text" name="house" />
+        <input type="submit" value="submit" />
+      </form>
     );
   }
 
+  render() {
+    return (
+      <div>
+        <button onClick={this.renderForm}>Add a new property</button>
+      </div>
+    );
+  }
 }
--- src/__tests__/Button.test.js
+++ src/__tests__/Button.test.js
@@ -6,12 +6,34 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import 'jest-enzyme';
 
-Enzyme.configure({ adapter: new Adapter() })
+Enzyme.configure({ adapter: new Adapter() });
 
-describe('Button', function () {
-  it('should render correctly', function () {
+describe('Button', function() {
+  it('should render correctly', function() {
     const wrapper = shallow(<Button />);
-    const button = <button>Add a new property</button>;
+    console.log('Button equals: ', Button);
+    const button = (
+      <button onClick={Button._this.renderForm}>Add a new property</button>
+    );
     expect(wrapper).toContainReact(button);
+  });
+
+  // it('renders a form when the button is clicked', () => {
+  //   const mockFn = jest.fn();
+  //   const btn = shallow(<button onClick={mockFn}>Add a new property</button>);
+  //   btn.simulate('click');
+  //   expect(mockFn).toHaveBeenCalled();
+  // });
+
+  it('has a function which renders a form', () => {
+    const wrapper = shallow(<Button />);
+    const form = (
+      <form>
+        <input type="text" name="house" />
+        <input type="submit" value="submit" />
+      </form>
+    );
+    wrapper.find('button').simulate('click');
+    expect(wrapper).toContainReact(form);
   });
 });
