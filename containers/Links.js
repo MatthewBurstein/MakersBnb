3c44e1311fd23a877d7438fdd6df5658aa009e67
Matt Burstein <j.matthew.burstein@gmail.com>
2018-03-02 14:12:47 +0000

    commit to switch driver

--- containers/Links.js
+++ containers/Links.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import axios from 'axios';
 
-export default class Test extends Component {
+export default class Links extends Component {
 
   constructor() {
     super();
--- src/__tests__/listImages.spec.js
+++ src/__tests__/listImages.spec.js
@@ -19,8 +19,10 @@ it('renders a message', () => {
   expect(wrapper).toContainReact(message);
 });
 
-it('shows an image from the database', () => {
+it('shows an image from the database', async () => {
+  expect.assertions(1);
   const wrapper = shallow(<Links />);
-  const image = <img src="https://img00.deviantart.net/c73f/i/2014/001/7/7/magic_house_by_tinca2-d70b7x2.jpg"/>;
-  expect(wrapper).toContainReact(image);
+  const image = <img src="https://img00.deviantart.net/c73f/i/2014/001/7/7/magic_house_by_tinca2-d70b7x2.jpg"></img>;
+  const page = await wrapper.renderImages();
+  expect(page).toContainReact(image);
 });
